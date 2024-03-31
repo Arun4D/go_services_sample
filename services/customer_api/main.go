7b0d1d98649b8021853a12c206e5e3353503346d
Arun Duraisamy <Arun4.Duraisamy@gmail.com>
2024-03-31 14:32:58 +0000

    changed album to customer

--- services/customer_api/main.go
+++ services/customer_api/main.go
@@ -6,63 +6,63 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
+// customer represents data about a record customer.
+type customer struct {
 	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	FirstName  string  `json:"first_name"`
+	LastName string  `json:"last_name"`
+	DateOfBirth  string `json:"date_of_birth"`
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+// customers slice to seed record customer data.
+var customers = []customer{
+	{ID: "1", FirstName: "Blue Train", LastName: "John Coltrane", DateOfBirth: "01-01-1988"},
+	{ID: "2", FirstName: "Jeru", LastName: "Gerry Mulligan", DateOfBirth: "01-01-1988"},
+	{ID: "3", FirstName: "Sarah Vaughan and Clifford Brown", LastName: "Sarah Vaughan", DateOfBirth: "01-01-1988"},
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+// getcustomers responds with the list of all customers as JSON.
+func getCustomers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, customers)
 }
 
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+// postcustomers adds an customer from JSON received in the request body.
+func postCustomers(c *gin.Context) {
+	var newCustomer customer
 
 	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+	// newcustomer.
+	if err := c.BindJSON(&newCustomer); err != nil {
 		return
 	}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	// Add the new customer to the slice.
+	customers = append(customers, newCustomer)
+	c.IndentedJSON(http.StatusCreated, newCustomer)
 }
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
+// getcustomerByID locates the customer whose ID value matches the id
+// parameter sent by the client, then returns that customer as a response.
+func getCustomerByID(c *gin.Context) {
 	id := c.Param("id")
 
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
+	// Loop over the list of customers, looking for
+	// an customer whose ID value matches the parameter.
+	for _, a := range customers {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "customer not found"})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
+	router.GET("/customers", getCustomers)
+	router.GET("/customers/:id", getCustomerByID)
 
-	router.POST("/albums", postAlbums)
+	router.POST("/customers", postCustomers)
 
 	router.Run("localhost:8080")
 }
